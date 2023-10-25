1c458636b3c0ee3241b7598ed61b78b54bb79bb1
Rajeev <rajeev.kr@infolitz.com>
2023-10-25 13:22:35 +0530

    initial commit

--- nRFPressureSensorCode/build_4/image_preload.cmake
+++
@@ -1,9 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/users/rajee/downloads/nrf52-sensors-feature_timestamp/nrf52-sensors-feature_timestamp/nrfpressuresensorcode;C:/zephyr/v2.4.2/nrf;C:/zephyr/v2.4.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/Users/rajee/Downloads/nrf52-sensors-Feature_timestamp/nrf52-sensors-Feature_timestamp/nRFPressureSensorCode/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/rajee/Downloads/nrf52-sensors-Feature_timestamp/nrf52-sensors-Feature_timestamp/nRFPressureSensorCode/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/rajee/Downloads/nrf52-sensors-Feature_timestamp/nrf52-sensors-Feature_timestamp/nRFPressureSensorCode/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
