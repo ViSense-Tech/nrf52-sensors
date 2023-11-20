9eefb290eb893a58f116ec655001e98f1e1ccba5
Adhil <adhilxavier@gmail.com>
2023-11-20 16:17:26 +0530

    Added uncommitted files to ti folder

---
+++ nRF_FlowMeter/ti/ccs1250/ccs/ccs_base/cloudagent/util/install-certs.bat
@@ -0,0 +1,6 @@
+certutil -addstore "TrustedPublisher" %~dp0\ti1.p7b
+certutil -addstore "TrustedPublisher" %~dp0\ti2.p7b
+certutil -addstore "TrustedPublisher" %~dp0\ti3.p7b
+certutil -addstore "TrustedPublisher" %~dp0\ti4.p7b
+certutil -addstore "TrustedPublisher" %~dp0\ti5.p7b
+certutil -addstore "TrustedPublisher" %~dp0\ti6.p7b
