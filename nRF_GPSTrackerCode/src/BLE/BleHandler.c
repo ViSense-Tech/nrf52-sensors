dbbdff36d348a8088290387a6a34ce7a7a6b0c62
Adhil <adhilxavier@gmail.com>
2023-12-07 17:20:46 +0530

    Updated advertising packet with 128 bit service UUID

--- nRF_GPSTrackerCode/src/BLE/BleHandler.c
+++ nRF_GPSTrackerCode/src/BLE/BleHandler.c
@@ -24,6 +24,10 @@ static const struct bt_data ad[] = {
 	BT_DATA(BT_DATA_NAME_SHORTENED, DEVICE_NAME, DEVICE_NAME_LEN),
 	// BT_DATA(BT_DATA_MANUFACTURER_DATA, ucAdVertsingBuffer, ADV_BUFF_SIZE)                                       
 };
+
+static const struct bt_data sd[] = {
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_CUSTOM_SERVICE_VAL)
+};
                                                                                                                         
 /**********************************FUNCTION DEFINITION****************/
 
@@ -96,8 +100,7 @@ int StartAdvertising(void)
 {
 	int nError = 0;
 
-	//nError = bt_le_ext_adv_start(adv, NULL); //enable for ext adv
-    nError = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
+    nError = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
 
 	if (nError) 
     {
