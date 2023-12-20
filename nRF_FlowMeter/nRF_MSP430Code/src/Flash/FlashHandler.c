8b8df9a0c809e45abd4cb3d3ede3d1dcae42e308
Adhil <adhilxavier@gmail.com>
2023-12-20 17:55:40 +0530

    Added support for mx66 flash in flow meter

--- nRF_FlowMeter/nRF_MSP430Code/src/Flash/FlashHandler.c
+++ nRF_FlowMeter/nRF_MSP430Code/src/Flash/FlashHandler.c
@@ -8,6 +8,10 @@
 /***************************************INCLUDES*********************************/
 #include "FlashHandler.h"
 
+
+/***************************************GLOBALS*********************************/
+static const struct device *const flash_dev = DEVICE_DT_GET(DT_NODELABEL(mx66l1g));
+
 /***************************************FUNCTION DECLARATION********************/
 int nvs_initialisation( struct nvs_fs *fs, uint8_t selector)
 {
@@ -99,4 +103,92 @@ int freeSpaceCalc(struct nvs_fs *fs, uint16_t data_count,uint16_t count_max)
 	int free_space = nvs_calc_free_space(fs);
 	printk("\n free space %d ", free_space);
 	return free_space;
+}
+
+/**
+ * @brief  Write data to external flash
+ * @param pcBuffer : data to send
+ * @param unLength : Length of data
+ * @return True for success
+*/
+bool writeJsonToExternalFlash(char *pcBuffer, uint32_t flashIdx, int unLength) // &flashIdx
+{
+	bool bRetval = false;
+	int rc = 0;
+	flashIdx = (flashIdx * WRITE_ALIGNMENT);
+
+	do
+	{
+		rc = flash_write(flash_dev, SPI_FLASH_REGION_OFFSET + flashIdx, pcBuffer, unLength);
+		if (rc != 0)
+		{
+			printf("Flash write failed! %d\n", rc);
+			break;
+		}
+		printk("offset:%d\n",flashIdx);
+		
+		k_sleep(K_MSEC(10));
+		bRetval = true;	
+	} while (0);
+
+	return bRetval;
+
+}
+/**
+ * @brief  Read data from external flash
+ * @param pcBuffer : data buffer to store
+ * @param unLength : Length of data
+ * @return True for success
+*/
+bool readJsonFromExternalFlash(char *pcBuffer, uint32_t flashIdx, int unLength)
+{
+	bool bRetval = false;
+	int rc = 0;
+	
+	// flashIdx = flashIdx * 256;
+	flashIdx = flashIdx * WRITE_ALIGNMENT;
+	
+
+	do
+	{
+		rc = flash_read(flash_dev,SPI_FLASH_REGION_OFFSET + flashIdx, pcBuffer, WRITE_ALIGNMENT);
+		if (rc != 0)
+		{
+			printf("Flash read failed! %d\n", rc);
+			break;
+		}
+		printk("offset:%d\n",flashIdx);
+		bRetval = true;
+		
+	}while(0);
+
+	return bRetval;
+
+}
+/**
+ * @brief  Erase external flash
+ * @param None
+ * @return True for success
+*/
+bool EraseExternalFlash(uint16_t uSectorIdx)
+{
+	bool bRetval = false;
+	int rc = 0;
+
+	do
+	{
+		rc = flash_erase(flash_dev, SPI_FLASH_REGION_OFFSET,
+				SPI_FLASH_SECTOR_SIZE * uSectorIdx);
+		if (rc != 0) 
+		{
+			printf("Flash erase failed! %d\n", rc);
+			break;
+		} else 
+		{
+			printf("Flash erase succeeded!\n");
+		}
+		bRetval = true;
+	} while (0);
+
+	return bRetval;
 }
--- nRF_FlowMeter/nRF_MSP430Code/src/main.c
+++ nRF_FlowMeter/nRF_MSP430Code/src/main.c
@@ -28,7 +28,7 @@ int32_t lDiagnosticdata = 0;
 _sConfigData sConfigData = {0};
 struct nvs_fs fs;    //file system
 struct nvs_fs sConfigFs;
-uint32_t uFlashIdx = 0;  // initialise data counter
+uint32_t ulFlashIdx = 0;  // initialise data counter
 
 /*************************FUNTION DECLARATION*****************/
 static bool InitPowerManager();
@@ -56,6 +56,7 @@ int main(void)
 	uint8_t ucIdx = 0;
 	uint8_t *pucAdvBuffer = NULL;
 	char *cJsonBuffer = NULL;
+    char cFlashReadBuf[350];
 	char cBuffer[30];
     char *pcEnd = NULL;
 	double dFlowRate = 0.0;
@@ -146,6 +147,14 @@ int main(void)
 			if(IsNotificationenabled())
 			{
 				VisenseSensordataNotify(ucNotifyBuffer, ADV_BUFF_SIZE);
+			}
+            else if (!IsConnected() && !IsNotificationenabled())
+			{
+				writeJsonToExternalFlash(cJsonBuffer, ulFlashIdx, WRITE_ALIGNMENT);
+				readJsonFromExternalFlash(cFlashReadBuf, ulFlashIdx, WRITE_ALIGNMENT);
+				printk("\n\rcFlash read%s\n\r", cFlashReadBuf);
+				ulFlashIdx++;
+				printk("flash count: %d\n\r", ulFlashIdx);
 			}
 
 			cJSON_Delete(pMainObject);
@@ -309,7 +318,7 @@ static bool CheckForConfigChange()
     else
     {
         SetSleepTime(sConfigData.sleepTime);
-        uFlashIdx = sConfigData.flashIdx;
+        ulFlashIdx = sConfigData.flashIdx;
         printk("sConfigFlag %d ,flashIdx = %d\n",
                                      sConfigData.flag,
                                      sConfigData.flashIdx); //get all the config params from the flash if a reboot occures
@@ -340,18 +349,21 @@ static bool SendHistoryDataToApp(char *pcBuffer, uint16_t unLength)
             
             memset(cBuffer, '\0', sizeof(cBuffer));
             memcpy(cBuffer, pcBuffer, unLength);
-            writeJsonToFlash(&fs, uFlashIdx, NUMBER_OF_ENTRIES, cBuffer, strlen(cBuffer));
-            k_msleep(50);
-            if (readJsonToFlash(&fs, uFlashIdx, NUMBER_OF_ENTRIES, cBuffer, strlen(cBuffer)))
+            if(writeJsonToExternalFlash(cBuffer, ulFlashIdx,WRITE_ALIGNMENT))
             {
-                printk("Read succes\n\r");
+                // NO OP
             }
-            uFlashIdx++;
-            sConfigData.flashIdx = uFlashIdx;
-            nvs_write(&sConfigFs, 0, (char *)&sConfigData, sizeof(_sConfigData));
-            if(uFlashIdx>= NUMBER_OF_ENTRIES)
+            k_msleep(50);
+            if (readJsonFromExternalFlash(cBuffer, ulFlashIdx, WRITE_ALIGNMENT))
             {
-                uFlashIdx = 0;
+                printk("\nId: %d, Stored_Data: %s\n",ulFlashIdx, cBuffer);
+            }
+            ulFlashIdx++;
+            sConfigData.flashIdx = ulFlashIdx;
+            nvs_write(&sConfigFs, 0, (char *)&sConfigData, sizeof(_sConfigData));
+            if(ulFlashIdx>= NUMBER_OF_ENTRIES)
+            {
+                ulFlashIdx = 0;
             }
         }
  
@@ -359,7 +371,7 @@ static bool SendHistoryDataToApp(char *pcBuffer, uint16_t unLength)
         if(IshistoryNotificationenabled() && IsConnected())
         {
             VisenseHistoryDataNotify();
-            uFlashIdx = 0; 
+            ulFlashIdx = 0; 
         }
 
         bRetval = true;
