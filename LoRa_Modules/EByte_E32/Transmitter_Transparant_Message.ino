2a4bbd94fdb83d6bf8b2764241390c12d848bc58
Rajeev <rajeev.kr@infolitz.com>
2024-01-05 03:02:59 +0530

    update

--- LoRa_Modules/EByte_E32/Transmitter_Transparant_Message.ino
+++ LoRa_Modules/EByte_E32/Transmitter_Transparant_Message.ino
@@ -19,57 +19,64 @@
 #include "Arduino.h"
 #include "LoRa_E32.h"
 // ---------- esp8266 pins --------------
-//LoRa_E32 e32ttl(RX, TX, AUX, M0, M1);  // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
-//LoRa_E32 e32ttl(D3, D4, D5, D7, D6); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX AUX M0 M1
-//LoRa_E32 e32ttl(D2, D3); // Config without connect AUX and M0 M1
+// LoRa_E32 e32ttl(RX, TX, AUX, M0, M1);  // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
+// LoRa_E32 e32ttl(D3, D4, D5, D7, D6); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX AUX M0 M1
+// LoRa_E32 e32ttl(D2, D3); // Config without connect AUX and M0 M1
 
-//SoftwareSerial mySerial(D2, D3); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
-//LoRa_E32 e32ttl(&mySerial, D5, D7, D6); // AUX M0 M1
-// -------------------------------------
-// ---------- Arduino pins --------------
-//LoRa_E32 e32ttl(4, 5, 3, 7, 6); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX AUX M0 M1
-//LoRa_E32 e32ttl(4, 5); // Config without connect AUX and M0 M1
-//#include <SoftwareSerial.h>
-//SoftwareSerial mySerial(4, 5); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
-//LoRa_E32 e32ttl(&mySerial, 3, 7, 6); // AUX M0 M1
-// -------------------------------------
-// ------------- Arduino MKR WiFi 1010 -------------
-// LoRa_E32 e220ttl(&Serial1, 1, 2, 3); //  RX AUX M0 M1
-// -------------------------------------------------
-// ---------- esp32 pins --------------
-//#include <HardwareSerial.h>
-//HardwareSerial Serial2(13, 12);
+// SoftwareSerial mySerial(D2, D3); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
+// LoRa_E32 e32ttl(&mySerial, D5, D7, D6); // AUX M0 M1
+//  -------------------------------------
+//  ---------- Arduino pins --------------
+// LoRa_E32 e32ttl(4, 5, 3, 7, 6); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX AUX M0 M1
+// LoRa_E32 e32ttl(4, 5); // Config without connect AUX and M0 M1
+// #include <SoftwareSerial.h>
+// SoftwareSerial mySerial(4, 5); // Arduino RX <-- e22 TX, Arduino TX --> e22 RX
+// LoRa_E32 e32ttl(&mySerial, 3, 7, 6); // AUX M0 M1
+//  -------------------------------------
+//  ------------- Arduino MKR WiFi 1010 -------------
+//  LoRa_E32 e220ttl(&Serial1, 1, 2, 3); //  RX AUX M0 M1
+//  -------------------------------------------------
+//  ---------- esp32 pins --------------
+// #include <HardwareSerial.h>
+// HardwareSerial Serial2(13, 12);
 LoRa_E32 e32ttl(&Serial2, 15, 21, 19); //  RX AUX M0 M1
-//LoRa_E32 e32ttl(&Serial2, 22, 4, 18, 21, 19, UART_BPS_RATE_9600); //  esp32 RX <-- e22 TX, esp32 TX --> e22 RX AUX M0 M1
-// -------------------------------------
-void setup() {
-  Serial.begin(9600);
-  delay(5000);
-  // Startup all pins and UART
-  e32ttl.begin();
-  Serial.println("Hi, I'm going to send message!");
-  // Send message
-  ResponseStatus rs = e32ttl.sendMessage("Hello, world?");
-  // Check If there is some problem of succesfully send
-  Serial.println(rs.getResponseDescription());
-  Serial.println("setup end");
+// LoRa_E32 e32ttl(&Serial2, 22, 4, 18, 21, 19, UART_BPS_RATE_9600); //  esp32 RX <-- e22 TX, esp32 TX --> e22 RX AUX M0 M1
+//  -------------------------------------
+void setup()
+{
+    Serial.begin(9600);
+    delay(5000);
+    // Startup all pins and UART
+    e32ttl.begin();
+    Serial.println("Hi, I'm going to send message!");
+    // Send message
+    ResponseStatus rs = e32ttl.sendMessage("Hello, world?");
+    // Check If there is some problem of succesfully send
+    Serial.println(rs.getResponseDescription());
+    Serial.println("setup end");
 }
-void loop() {
+void loop()
+{
     // If something available
-  if (e32ttl.available()>1) {
-      // read the String message
-    ResponseContainer rc = e32ttl.receiveMessage();
-    // Is something goes wrong print error
-    if (rc.status.code!=1){
-        Serial.println(rc.status.getResponseDescription());
-    }else{
-        // Print the data received
-        Serial.println(rc.status.getResponseDescription());
-        Serial.println(rc.data);
+    if (e32ttl.available() > 1)
+    {
+        // read the String message
+        ResponseContainer rc = e32ttl.receiveMessage();
+        // Is something goes wrong print error
+        if (rc.status.code != 1)
+        {
+            Serial.println(rc.status.getResponseDescription());
+        }
+        else
+        {
+            // Print the data received
+            Serial.println(rc.status.getResponseDescription());
+            Serial.println(rc.data);
+        }
     }
-  }
-  if (Serial.available()) {
-      String input = Serial.readString();
-      e32ttl.sendMessage(input);
-  }
+    if (Serial.available())
+    {
+        String input = Serial.readString();
+        e32ttl.sendMessage(input);
+    }
 }
--- LoRa_Modules/Reyax/Transmiter.ino
+++ LoRa_Modules/Reyax/Transmiter.ino
@@ -1,17 +1,15 @@
 char txPacket[30];
-
 float txNumber;
 
-void setup() {
-  Serial2.begin(115200); //set baud rate of uart2
-  txNumber = 0;
+void setup()
+{
+    Serial2.begin(115200); // set baud rate of uart2
+    txNumber = 0;
 }
-void loop() {
-  sprintf(txPacket, "MESSAGE-NUMBER %0.2f \r\n", txNumber);
-  Serial2.printf("AT+SEND=101, %d, %s", strlen(txPacket), txPacket);
-  txNumber++;
-  delay(2000);
- 
+void loop()
+{
+    sprintf(txPacket, "MESSAGE-NUMBER %0.2f \r\n", txNumber);
+    Serial2.printf("AT+SEND=101, %d, %s", strlen(txPacket), txPacket);
+    txNumber++;
+    delay(2000);
 }
-
-  
